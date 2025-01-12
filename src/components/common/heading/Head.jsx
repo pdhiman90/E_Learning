6f933d5d8c5bf80601db87ef3388a5da4531aa4e
Prince <princediman73573@gmail.com>
2025-01-12 11:11:49 +0530

    first commit

---
+++ src/components/common/heading/Head.jsx
@@ -0,0 +1,22 @@
+const Head = ()=>{
+    return(
+        <div>
+        <section className="head">
+            <div className="container flexSB">
+                <div className="logo">
+                    <h1>ACDAEMIA</h1>
+                    <span>Online Education & Learning</span>
+                </div>
+                <div className="social">
+                <i className="fab fa-facebook-f icon"></i>
+                <i className="fab fa-instagram icon"></i>
+                <i className="fab fa-twitter icon"></i>
+                <i className="fab fa-youtube icon"></i>
+                </div>
+            </div>
+        </section>
+    </div>
+    )
+}
+
+export default Head;
---
+++ src/components/common/title/Title.jsx
@@ -0,0 +1,12 @@
+const Title = ({subtitle, title}) => {
+    return (
+        <div>
+            <div id="heading">
+                <h3>{subtitle}</h3>
+                <h1>{title}</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Title;
